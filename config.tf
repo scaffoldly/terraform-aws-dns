4a094a10c8f614ab217c94b5c1364b23cc926cfd
Christian Nuss <cnuss@users.noreply.github.com>
2021-07-09 16:16:37 -0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -4,6 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
+      version               = "~> 3.33.0"
       configuration_aliases = [aws.dns]
     }
   }
